94c8ea9b2d0da7752eaf65bf797fc98ff714e54b
NetkoNefarious <jmagazin5@gmail.com>
2018-08-18 15:27:48 +0200

    Added smooth scrolling for links

--- js/script.js
+++ js/script.js
@@ -153,3 +153,19 @@ document.getElementById('date2').onkeydown = function (event) {
 }
 
 //#endregion
+
+// Smooth-scrolling links
+$('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+        }, 1000);
+        return false;
+        }
+    }
+});
